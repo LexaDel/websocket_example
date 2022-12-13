b9c82579ca60afe51a7028ec9e915398f76226a4
Alexei Delejov <lexadelejov@gmail.com>
2022-12-13 14:07:38 +0300

    feat: add routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,28 @@
 import { Component } from "react";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+    {
+        path: "/admin",
+        element: <div> Hello Admin </div>,
+    },
+    {
+        path: "/doctor",
+        element: <div> Hello Doctor </div>,
+    }
+]);
 export class App extends Component {
     render() {
-        return <div> Hello World </div>
+        return (
+            <RouterProvider router={router} />
+        )
     }
 }
