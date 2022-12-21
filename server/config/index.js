9030b5e4664edebf3cf169456b16bdf683dcc861
Alexei Delejov <lexadelejov@gmail.com>
2022-12-21 17:22:22 +0300

    feat: impliment auth for users

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,14 @@
 const {
-    DB_USER,
-    DB_PASSWORD,
-    DB_HOST,
-    DB_PORT,
-    DB_NAME,
-  } = process.env;
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+} = process.env;
 
-export const MONGODB_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+export const MONGODB_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+export const ALLOWED_ORIGIN = 'http://localhost:3000';
+export const COOKIE_NAME = 'qid';
+export const ACCESS_TOKEN_SECRET = 'access_token_secret';
+export const SENTRY_DSN = 'sentry_dsn';
+export const VERIFICATION_CODE = 'verification_code';
--- server/index.js
+++ server/index.js
@@ -1,22 +1,45 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import express, { json, urlencoded } from 'express';
+import mongoose from 'mongoose';
 import {
+  ALLOWED_ORIGIN,
   MONGODB_URI   
-} from './config/index.js'
+} from './config/index.js';
+import { setSecurityHeaders } from './middlewares/index.js';
+import router from './routes/app.routes.js';
 
 const app = express();
+app.use(
+  cors({
+    origin: ALLOWED_ORIGIN,
+    credentials: true
+  })
+);
+app.use(setSecurityHeaders);
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(cookieParser());
 
 try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    console.log('🚀 Connected to DB')
-  } catch (e) {
-    console.log(`Error while connecting to DB: ${e}`)
-  }
+  await mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  console.log('🚀 Connected to DB')
+} catch (e) {
+  console.log(`Error while connecting to DB: ${e}`)
+}
 
-  const PORT = process.env.NODE_DOCKER_PORT || 8080;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-  });
+app.use('/api', router);
+
+app.use((err, req, res) => {
+  console.log(`${err.message || JSON.stringify(err, null, 2)}`);
+  res.status(500).json({ message: 'Something went wrong. Try again later' });
+});
+
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
